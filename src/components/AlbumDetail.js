2fc49562fa7598e9eeb39fba6c3e74e19b0cddcb
Joe Stowers <greenchiletexan@gmail.com>
2018-03-04 03:37:34 -0600

    AlbumDetail: build out component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,7 +1,20 @@
 // AlbumDetail Component
 // March 4, 2018
 
-/* 
-	Purpose:
-	1. display card for a single album
-*/
+// Purpose: display card for a single album
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumDetail = (props) => {
+
+	const { album } = props;
+
+	return (
+		<View>	
+			<Text>{ album.title }</Text>
+		</View>
+	)
+}
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -11,6 +11,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
@@ -25,11 +26,19 @@ class AlbumList extends Component {
 			});
 	}
 
+	displayAlbums(albums) {
+		return albums.map(album => 
+			<AlbumDetail key={album.title} album={album} />
+		);
+	}
+
 	render() {
-		if (this.state.albums.length > 0) {
+		const { albums } = this.state;
+
+		if (albums.length > 0) { 
 			return (
 				<View>
-					<Text>Album List!!</Text>
+					{ this.displayAlbums(albums) }
 				</View>
 			);
 		}
